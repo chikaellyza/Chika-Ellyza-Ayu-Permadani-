bd5ad9fc7f7dcd52852176de66496619ff0fdf09
chikaellyza <ellyzacantik36@gmail.com>
2026-07-28 11:27:40 +0700

    Implementasi routing React

--- react-component/src/App.jsx
+++ react-component/src/App.jsx
@@ -1,10 +1,23 @@
-import Welcome from "./components/Welcome";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div>
-      <Welcome />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
