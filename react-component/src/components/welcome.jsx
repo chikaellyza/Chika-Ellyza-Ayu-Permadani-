6bd7900f457076eb00cb17c2063a6e482190d106
chikaellyza <ellyzacantik36@gmail.com>
2026-07-28 08:53:24 +0700

    Membuat contoh component React

---
+++ react-component/src/components/welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome() {
+  return (
+    <div>
+      <h1>Belajar React Component</h1>
+      <p>Ini adalah contoh component sederhana.</p>
+    </div>
+  );
+}
+
+export default Welcome;
